ab98b95ff399621998bd611d9a561b5253cb8bb0
Marcus Pöhls <marcus.poehls@gmail.com>
2019-06-07 14:56:22 +0200

    initial

---
+++ test/registers-classes-as-lifecycle-extensions.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const Hapi = require('@hapi/hapi')
+const { expect } = require('@hapi/code')
+
+const { experiment, it, beforeEach } = (exports.lab = Lab.script())
+
+experiment('hapi-class-extension-points plugin', () => {
+  beforeEach(async () => {
+    this.server = new Hapi.Server()
+
+    await this.server.register({
+      plugin: require('../lib/index')
+    })
+
+    await this.server.initialize()
+  })
+
+  it('server has ".extClass" decoration', async () => {
+    expect(this.server.extClass).to.exist()
+  })
+
+  it('registers class with extension points', async () => {
+    class TestExtension {
+      onRequest (request, h) {
+        request.setUrl('/testing')
+
+        return h.continue
+      }
+    }
+
+    this.server.extClass(TestExtension)
+
+    this.server.route({
+      path: '/testing',
+      method: 'GET',
+      handler: request => request.path
+    })
+
+    const request = {
+      url: '/',
+      method: 'GET'
+    }
+
+    const response = await this.server.inject(request)
+    expect(response.statusCode).to.equal(200)
+    expect(response.payload).to.equal('/testing')
+  })
+
+  it('throws for non-class parameter', async () => {
+    const extension = function onRequest () {}
+    const throws = () => this.server.extClass(extension)
+    expect(throws).to.throw()
+  })
+
+  it('throws when missing methods extending the request lifecycle', async () => {
+    class TestExtension {
+      constructor (server) {
+        this.server = server
+      }
+    }
+
+    const throws = () => this.server.extClass(TestExtension)
+    expect(throws).to.throw()
+  })
+})
